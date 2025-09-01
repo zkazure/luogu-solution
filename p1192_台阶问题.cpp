71be25f751ee600b90d7dffb7a48ae98638446f9
Kazure Zheng <kazure@debian>
2025-09-01 15:56:41 +0800

    ac

--- p1192_台阶问题.cpp
+++ p1192_台阶问题.cpp
@@ -1,3 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int dp[100005] = {0};
+    const int Mod = 100003;
+    dp[0] = 1;
+    int n, k;
+    cin >> n >> k;
+
+    for (int i=i; i<=n; ++i) {
+        for (int j=1; j<=k; ++j) {
+            if (i>=j) {
+                dp[i] = (dp[i] + dp[i-j]) % Mod;
+                // dp[i] += dp[i-j]; 
+            }
+            // dp[i] %= Mod;
+            // This is wrong, because it will overflow. 
+        }
+    }
+    dp[n] %= Mod;
+    cout << dp[n];
+    return 0;
+}
+
 // ===== my solution2  =====
 // it is too easy to TLE
 #include <bits/stdc++.h>
